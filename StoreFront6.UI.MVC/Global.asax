75ba77d140b59821e8aa89b18ff1f5d4946b70a1
Jordan Hickman <jdhickman816@outlook.com>
2021-04-26 20:39:26 -0500

    First Commit

---
+++ StoreFront6.UI.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StoreFront6.UI.MVC.MvcApplication" Language="C#" %>
---
+++ StoreFront6.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoreFront6.UI.MVC.Startup))]
+namespace StoreFront6.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
