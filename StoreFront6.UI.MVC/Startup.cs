e5ea397d4c6f08cec77f325ce3ab1abe0d1af616
Jordan Hickman <jdhickman816@outlook.com>
2021-04-26 20:50:34 -0500

    Added IS Added XML file in a new folder called configs, XML file is named Connections.config, pasted the ConnectionString section from Web.config Changed the Data Source, Initial Catalog, and Integrated Security route Added existing item .gitignore, added at the bottom:
    # Protect sensitive data by excluding config folder
    /StoreFront6.UI.MVC/config

--- StoreFront6.UI.MVC/Startup.cs
+++ StoreFront6.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(StoreFront6.UI.MVC.Startup))]
 namespace StoreFront6.UI.MVC
 {
     public partial class Startup
